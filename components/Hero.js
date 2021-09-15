534ba7feff495e41f119b0337c95fabda10141a9
Steve <stevekentlanier@gmail.com>
2021-09-15 12:08:27 -0500

    changes

--- components/Hero.js
+++ components/Hero.js
@@ -1,13 +1,7 @@
 const Hero = () => (
-    <div className="md:flex md:flex-row -mt-28">
-        <div className="bg-gray-100 text-center md:w-1/1">
-            <img
-            className="object-contain mx-auto"
-            alt="Mission Control"
-            src="/assets/img/Steves-Desk.jpg"
-            />
-        </div>
-    </div>
+    <div className="bg-hero-image bg-cover bg-center bg-no-repeat bg-fixed h-screen">
+        
+    </div>  
 );
 
 export default Hero;
--- components/Layout.js
+++ components/Layout.js
@@ -2,25 +2,25 @@ import Header from "./Header";
 import Nav from "./Nav";
 import Hero from "./Hero";
 
-const layoutStyle = {
-  display: "flex",
-  flexDirection: "column",
-  height: "100%",
-  width: "100%"
-};
+// const layoutStyle = {
+//   display: "flex",
+//   flexDirection: "column",
+//   height: "100%",
+//   width: "100%"
+// };
 
-const contentStyle = {
-  flex: 1,
-  display: "flex",
-  flexDirection: "column"
-};
+// const contentStyle = {
+//   flex: 1,
+//   display: "flex",
+//   flexDirection: "column"
+// };
 
 const Layout = props => (
-  <div className="Layout" style={layoutStyle}>
+  <div className="Layout">
     <Nav/>
     <Hero/>
     <Header/>
-    <div className="Content" style={contentStyle}>
+    <div className="Content">
       {props.children}
     </div>
     
