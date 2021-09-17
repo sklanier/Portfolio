49bcb13cc5d0f9faff1e1cd35bbb9bae77bc2d25
Steve <stevekentlanier@gmail.com>
2021-09-17 10:14:24 -0500

    changed nav

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,8 @@
 const Hero = () => (
     <div className="bg-hero-image bg-cover bg-center bg-no-repeat bg-fixed h-screen">
-        
+        <div className="text-center m-auto">
+            I'm Steve, and I like to
+        </div>
     </div>  
 );
 
