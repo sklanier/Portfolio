e31c0bb26a0df5be69dd0c4b1040cdc186830b69
Steve <stevekentlanier@gmail.com>
2021-09-21 15:54:16 -0500

    extended theme.height.inherit to center hero text

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,7 @@
 const Hero = () => (
     <div className="h-screen bg-hero-image bg-fixed bg-cover bg-center bg-no-repeat">
-        <div className="text-center m-auto">
-            I'm Steve, and I like to
+        <div className="text-xl text-white flex justify-center items-center m-auto h-inherit">
+            <h1>I'm Steve, and I like to</h1>
         </div>
     </div>  
 );
