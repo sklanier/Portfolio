817b2210cd88f9d9b25f38a87b68c550c64a4619
Steve <stevekentlanier@gmail.com>
2021-09-22 14:10:18 -0500

    added custom utility class navText

--- components/Nav.js
+++ components/Nav.js
@@ -11,9 +11,11 @@ const Nav = () => (
   <nav className="bg-transparent fixed top-0 left-0 right-0">
     <div className="bg-transparent flex justify-around w-8/12 m-auto" style={navStyles.navBar}>
       <img src="assets/img/Github-icon.svg"/>
-      <Link href="#about"><a className="text-white font-avenir">ABOUT</a></Link>
-      <Link href="#projects"><a className="text-white font-avenir">PROJECTS</a></Link>
-      <Link href="#connect"><a className="text-white font-avenir">CONNECT</a></Link>
+      <div className="flex justify-evenly w-4/12">
+        <Link href="#about"><a className="text-white navText">ABOUT</a></Link>
+        <Link href="#projects"><a className="text-white navText">PROJECTS</a></Link>
+        <Link href="#connect"><a className="text-white navText">CONNECT</a></Link>
+      </div>
       <img src="assets/img/Linkedin-icon.svg"/>
     </div>
   </nav>
