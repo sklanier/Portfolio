fe1884e4b6fae5d4c1e9e003bd823c9e4b6fb3c1
Steve <stevekentlanier@gmail.com>
2021-09-22 11:26:20 -0500

    nav changes

--- components/Nav.js
+++ components/Nav.js
@@ -8,17 +8,14 @@ const navStyles = {
 }
 
 const Nav = () => (
-  <nav>
-    <div className="github"></div>
-
+  <nav className="bg-transparent fixed top-0 left-0 right-0">
     <div className="bg-transparent flex justify-around w-8/12 m-auto" style={navStyles.navBar}>
       <img src="assets/img/Github-icon.svg"/>
-      <Link href="#about"><a>About</a></Link>
-      <Link href="#projects"><a>Projects</a></Link>
-      <Link href="#connect"><a>Connect</a></Link>
+      <Link href="#about"><a className="text-white">About</a></Link>
+      <Link href="#projects"><a className="text-white">Projects</a></Link>
+      <Link href="#connect"><a className="text-white">Connect</a></Link>
       <img src="assets/img/Linkedin-icon.svg"/>
     </div>
-
   </nav>
 );
 
