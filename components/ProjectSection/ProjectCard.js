97e70a3c26377835b6164449becb968012c4bb05
Steve <stevekentlanier@gmail.com>
2021-09-27 10:42:54 -0500

    added component rules

--- components/ProjectSection/ProjectCard.js
+++ components/ProjectSection/ProjectCard.js
@@ -1,5 +1,22 @@
 import Overlay from '../Overlay';
 
+
+/*
+Rules for DECLARING a React component:
+    - Name is capitalized
+    - Takes an object ("props") as its one argument
+    - Must return SOMETHING (null, empty string, elements, ...)
+    - React has built-in components that return HTML elements (h1, div, span...)
+    - Attribute names with dashes are camelCased (except data- & aria-)
+    - We can INTERPOLATE js expressions using curly brackets
+    - We interpolate attribute values and content
+
+
+Rules for USING a React component:
+    - Components are used (invoked) to obtain elements
+    - Instead of invoking with '( )' we use '< />'
+    - Arguments are passed as attributes
+*/ 
 const ProjectCard = ({project}) => (
     
     <div style={{
