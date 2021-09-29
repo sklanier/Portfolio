bb79eeaacdde2dc550bd6e9820a26b30e30f2e77
Steve <stevekentlanier@gmail.com>
2021-09-29 10:06:52 -0500

    added contact content, smooth scroll anchor

--- components/FooterSection/Contact.js
+++ components/FooterSection/Contact.js
@@ -1,6 +1,15 @@
 const Contact = () => (
     <section id="contact" className="h-footerHeight bg-footer-image">
-
+        <div className="text-xl text-white flex justify-center items-center m-auto h-inherit">
+            <div className="text-center w-5/12">
+                <h2 style={{ marginBottom: '30px' }}>let's make a connection</h2>
+                <hr width="50px" className="m-auto"></hr>
+                <h1 className="heroSub">Shoot me an email or give me a call. I love meeting new people!</h1>
+                <div className="flex">
+                    
+                </div>
+            </div>
+        </div>
     </section>
 )
 
