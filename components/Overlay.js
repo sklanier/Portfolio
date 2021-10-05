7e9f6f214081f001c072ba0d5a21d2a85e76777e
Steve <stevekentlanier@gmail.com>
2021-10-05 18:18:17 -0500

    changed z-index of overlay

--- components/Overlay.js
+++ components/Overlay.js
@@ -6,7 +6,7 @@ const Overlay = styled.div({
     position: "absolute",
     top: "0",
     width: "100%",
-    zIndex: 2,
+    zIndex: 3,
     transition: "background-color 350ms ease",
     backgroundColor: "transparent",
     padding: "20px 20px 0 20px",
