8059a5483adb32b7aea0694f5118109b28a2ca43
Steve <stevekentlanier@gmail.com>
2021-10-18 13:00:18 -0500

    changed card layout

--- components/ProjectSection/Projects.js
+++ components/ProjectSection/Projects.js
@@ -2,13 +2,13 @@ import { project_data } from '../../data/project_data';
 import ProjectCard from './ProjectCard';
 
 const Projects = () => (
-    <section id="projects" className="bg-black">
-        <div className="flex justify-center aboutTitle">
-            <h1 className="px-6 pt-4">projects</h1>
+    <section id="projects" className="bg-black px-16">
+        <div className="flex justify-around aboutTitle">
+            <h1 className="pt-4">projects</h1>
             <hr className="w-8/12 mt-16"/>
-            <h2 className="px-6 pt-4">my github</h2>
+            <h2 className="pt-4">my github</h2>
         </div>
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-0">
+        <div className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-0">
             {project_data.map((project) => (
                 <ProjectCard key={project.id} project={project} />
             ))}
