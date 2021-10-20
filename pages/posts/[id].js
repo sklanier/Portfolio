1b2f106a0e15f4d3ebce09d140599350fff4bd69
Steve <stevekentlanier@gmail.com>
2021-10-20 14:03:44 -0500

    added code to [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,13 @@
 import Layout from '../../components/Layout'
+import { getAllPostIds } from '../../lib/posts'
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
 
 export default Post = () => (
     <Layout>...</Layout>
