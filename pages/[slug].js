88b18eee9159e04dc88a708fd0c7ae3b0dc6b9d0
Steve <stevekentlanier@gmail.com>
2021-10-25 12:57:59 -0500

    lost

--- pages/[slug].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router"
-
-export default function DynamicPage() {
-  const router = useRouter()
-  const {
-    query: { id },
-  } = router
-  return <div>The dynamic route is {id}</div>
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,5 @@
-import Link from 'next/link'
+import Layout from "../components/Layout";
 
-export default function IndexPage() {
-    return (
-        <ul>
-            <li>
-                <Link href="/">
-                    <a>Home</a>
-                </Link>
-            </li>
-            <li>
-                <Link href="/[slug]" as="/my-slug">
-                    <a>First Route</a>
-                </Link>
-            </li>
-            <li>
-                <Link href="/my-folder/[id]" as="/my-folder/my-id">
-                    <a>Second Route</a>
-                </Link>
-            </li>
-        </ul>
-    )
-}
+const Index = () => <Layout></Layout>;
+
+export default Index;
