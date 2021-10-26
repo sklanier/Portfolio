8afaebee17c31f8b1416346d106837547826bde8
Steve <stevekentlanier@gmail.com>
2021-10-26 14:08:08 -0500

    centered project image

--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -3,15 +3,17 @@ import { useRouter } from "next/router";
 export default function ProjectPage() {
    const router = useRouter();
    return (
-      <div className="flex-1">
-         <div className="w-6/12">
-            <img src={router.query.image}/>
+      <section id="projectWrapper" className="bg-black h-screen">
+         <div className="flex mx-20 h-screen">
+            <div className="w-6/12 flex items-center">
+               <img src={router.query.image}/>
+            </div>
+
+            <div className="w-6/12">
+               <h1 className="text-2xl font-bold">{router.query.title}</h1>
+               <p>{router.query.description}</p>
+            </div>
          </div>
-         
-         <div className="w-6/12">
-            <h1 className="text-2xl font-bold">{router.query.title}</h1>
-            <p>{router.query.description}</p>
-         </div>
-      </div>
+      </section>
    );
 }
