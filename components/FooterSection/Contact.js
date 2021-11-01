8d9cbd30be379d4d9d79357641c52f44d37be096
Steve <stevekentlanier@gmail.com>
2021-11-01 11:32:23 -0500

    made hr wider

--- components/FooterSection/Contact.js
+++ components/FooterSection/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => (
         <div className="text-xl text-white flex justify-center items-center m-auto h-inherit">
             <div className="text-center w-5/12">
                 <h2 style={{ marginBottom: '30px' }}>let's make a connection</h2>
-                <hr width="60px"  className="m-auto border-primaryBlue"></hr>
+                <hr width="120px"  className="m-auto border-primaryBlue"></hr>
                 <h1 className="heroSub">Shoot me an email or give me a call. I love meeting new people!</h1>
                 <div className="flex justify-around">
                     <div>
