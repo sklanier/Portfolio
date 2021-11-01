176bbac7879b13cb574cceee939d8c2923443520
Steve <stevekentlanier@gmail.com>
2021-11-01 12:01:52 -0500

    reverted project route

--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -4,32 +4,22 @@ import { useState, useEffect } from "react";
 
 const Nav = dynamic(() => import('../../components/Nav'), { ssr: false });
 
-if (typeof window !== 'undefined') {
-const [project] = useState(localStorage.getItem('project'));
-}
 
 function ProjectPage() {
    const router = useRouter();
-   
-   if (typeof window !== 'undefined') {
-   useEffect(() => {
-      
-         localStorage.setItem('project', router.query.project);
-   }, [router.query.project]);
-   }
 
    return (
       <section id="projectWrapper" className="bg-black h-screen">
          <Nav />
          <div className="flex mx-40 h-screen">
             <div className="w-6/12 flex items-center px-10">
-               <img src={project.image} />
+               <img src={router.query.image} />
             </div>
             <div className="w-6/12 flex flex-col justify-center px-20">
                <div>
-                  <h1 className="text-2xl text-white font-bold mb-10">{project.title}</h1>
+                  <h1 className="text-2xl text-white font-bold mb-10">{router.query.title}</h1>
                </div>
-               <p className="text-white projectDesc">{project.description}</p>
+               <p className="text-white projectDesc">{router.query.description}</p>
             </div>
          </div>
       </section>
