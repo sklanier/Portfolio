5b42bc41131f7335bc1fd49d87bb6a6b84fc8a9d
Steve <stevekentlanier@gmail.com>
2021-11-04 10:58:52 -0500

    removed unused code in layout

--- components/Layout.js
+++ components/Layout.js
@@ -6,18 +6,6 @@ import Contact from "./FooterSection/Contact";
 import PostFooter from "./FooterSection/PostFooter";
 import Scripts from './Scripts'
 
-// const layoutStyle = {
-//   display: "flex",
-//   flexDirection: "column",
-//   height: "100%",
-//   width: "100%"
-// };
-
-// const contentStyle = {
-//   flex: 1,
-//   display: "flex",
-//   flexDirection: "column"
-// };
 
 const Layout = () => (
   <div className="Layout">
