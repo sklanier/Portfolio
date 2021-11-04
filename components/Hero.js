286924c43c17f27f8523c4119b48a0521f37457a
Steve <stevekentlanier@gmail.com>
2021-11-04 11:32:40 -0500

    changed hero copy

--- components/Hero.js
+++ components/Hero.js
@@ -5,10 +5,14 @@ const Hero = () => (
         <div id="hero" className="h-screen bg-hero-image bg-fixed bg-cover bg-center bg-no-repeat">
             <div className="text-xl text-white flex justify-center items-center m-auto h-screen">
                 <div className="text-center w-6/12 mt-20">
-                    <h2>I'm steve, and I like to</h2>
-                    <h1 className="heroText">develop <span className="font-avenir">+</span> chill</h1>
+                    <h2>I'm steve. I like to</h2>
+                    <h1 className="heroText">
+                        make stuff.
+                        {/* <span className="font-avenir">+</span> */}
+                        
+                    </h1>
                     <hr width="100px" className="m-auto"></hr>
-                    <h1 className="heroSub leading-5">currently a full stack developer in Wisconsin, but I'm willing to relocate for the right job. a few things i enjoy... breaking things. fixing things. learning things. building things. (not necessarily in that order.)</h1>
+                    <h1 className="heroSub leading-5">currently a full stack developer in Wisconsin. a few things i enjoy: breaking things. fixing things. learning things. building things. (not necessarily in that order.)</h1>
                     <AnchorLink className="heroBtn" href="#projects">view my projects</AnchorLink>
                 </div>
             </div>
