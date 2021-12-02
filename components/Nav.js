5b44274db6656118527a5ba752c662666df07244
sklanier <stevekentlanier@gmail.comr>
2021-12-01 19:06:57 -0600

    project data added

--- components/Nav.js
+++ components/Nav.js
@@ -7,8 +7,6 @@ const navStyles = {
   }
 }
 
-//maybe blank spaces don't get counted
-
 const Nav = () => (
   <nav id="nav" className="transition duration-500 z-10 bg-transparent fixed top-0 left-0 right-0">
     <div className="bg-transparent flex justify-around w-8/12 m-auto" style={navStyles.navBar}>
