033e0cabd96fbbc401a1538757a375107f311957
sklanier <stevekentlanier@gmail.com>
2022-02-17 12:00:46 -0600

    removed comments

--- components/ProjectSection/ProjectCard.js
+++ components/ProjectSection/ProjectCard.js
@@ -1,19 +1,3 @@
-/*
-Rules for DECLARING a React component:
-    - Name is capitalized
-    - Takes an object ("props") as its one argument
-    - Must return SOMETHING (null, empty string, elements, ...)
-    - React has built-in components that return HTML elements (h1, div, span...)
-    - Attribute names with dashes are camelCased (except data- & aria-)
-    - We can INTERPOLATE js expressions using curly brackets
-    - We interpolate attribute values and content
-
-
-Rules for USING a React component:
-    - Components are used (invoked) to obtain elements
-    - Instead of invoking with '( )' we use '< />'
-    - Arguments are passed as attributes
-*/ 
 import Link from 'next/link'
 
 const ProjectCard = ({project}) => (
