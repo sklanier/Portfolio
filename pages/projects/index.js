4c373dff1b99a04478e40e1069ff82d221541c82
sklanier <stevekentlanier@gmail.com>
2022-03-04 13:10:38 -0600

    added nav to projects index

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,5 +1,6 @@
 import { project_data } from '../../data/project_data';
 import ProjectCard from '../../components/ProjectSection/ProjectCard';
+import Nav from '../../components/Nav';
 
 export const getStaticProps = async () => {
     return {
@@ -10,21 +11,24 @@ export const getStaticProps = async () => {
 };
 
 const Projects = ({ project }) => (
-    <section id="projects" className="bg-black px-28 -mt-10 pt-10">
-        <div className="flex justify-between aboutTitle py-8 px-4">
-            <h1 className="pt-4">projects</h1>
-            <hr className="w-8/12 mt-16"/>
-            <div className="flex w-1/6">
-                <img className="w-2/12" src="assets/img/Github-icon.svg"/>
-                <h2 className="pt-8 text-2xl">See my Github</h2>
+    <>
+        <Nav/>
+        <section id="projects" className="bg-black px-28 -mt-10 pt-10">
+            <div className="flex justify-between aboutTitle py-8 px-4">
+                <h1 className="pt-4">projects</h1>
+                <hr className="w-8/12 mt-16"/>
+                <div className="flex w-1/6">
+                    <img className="w-2/12" src="assets/img/Github-icon.svg"/>
+                    <h2 className="pt-8 text-2xl">See my Github</h2>
+                </div>
             </div>
-        </div>
-        <div className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-0">
-            {project.map((p) => (
-                <ProjectCard key={p.id} project={p}/>
-            ))}
-        </div>
-    </section>
+            <div className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-0">
+                {project.map((p) => (
+                    <ProjectCard key={p.id} project={p}/>
+                ))}
+            </div>
+        </section>
+    </>
 );
 
 export default Projects
