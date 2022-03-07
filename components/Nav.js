2cc6941056cd0253eeae4d2507484eb5821ab2a9
sklanier <stevekentlanier@gmail.com>
2022-03-07 17:42:48 -0600

    going back to original design

--- components/Nav.js
+++ components/Nav.js
@@ -10,12 +10,14 @@ const navStyles = {
 }
 
 const Nav = () => {
+
   const router = useRouter();
+
   return (
     <nav id="nav" className="transition duration-500 z-10 bg-transparent fixed top-0 left-0 right-0">
       <div className="bg-transparent flex justify-around w-8/12 m-auto" style={navStyles.navBar}>
         <a href="https://www.github.com/sklanier" target="_blank"><img src="../assets/img/Github-icon.svg"/></a>
-
+        
         {router.asPath === '/' ?
           <div className="flex justify-evenly w-3/12">
             <AnchorLink className="transition duration-500 navText" href="#about">ABOUT</AnchorLink>
