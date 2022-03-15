c0e6ef56e711600a22f5af4752b2c7488a314745
sklanier <stevekentlanier@gmail.com>
2022-03-15 17:38:50 -0500

    fixed tw global styles errors

--- components/Nav.js
+++ components/Nav.js
@@ -16,6 +16,7 @@ function NavLink({to, children}) {
   </a>
 }
 
+
 const MobileNav = ({ open, setOpen }) => {
   return (
     <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
