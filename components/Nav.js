f46a1216d71d5b903c8646dc6a4857eb3762f99d
sklanier <stevekentlanier@gmail.com>
2022-03-23 17:48:08 -0500

    fire giant battle

--- components/Nav.js
+++ components/Nav.js
@@ -18,9 +18,9 @@ function NavLink({to, children}) {
 
 const MobileNav = ({ open, setOpen }) => {
   return (
-    <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
+    <div className={`absolute top-0 left-0 h-screen w-screen bg-primaryBlue transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
         <div className="flex items-center justify-center filter drop-shadow-md bg-white h-20"> {/*logo container*/}
-            <a className="text-xl font-semibold" href="/">LOGO</a>
+            <a className="text-xl font-semibold" href="/">nav</a>
         </div>
         <div className="flex flex-col ml-4">
             <a className="text-xl font-medium my-4" href="/about" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
@@ -106,9 +106,9 @@ const Nav = () => {
                 setOpen(!open)
             }}>
                 {/* hamburger button */}
-                <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
-                <span className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
-                <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
+                <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
+                <span className={`h-1 w-full bg-white rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
+                <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
             </div>
 
             <div className="hidden md:flex">
